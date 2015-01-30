5541ecf8b227c16a4b444a479f6b6c43acce9db9
Samuel Shih <samshih@yahoo-inc.com>
2015-01-29 16:09:20 -0800

    Initial Commit

---
+++ Facebook Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facebook Demo
+//
+//  Created by Samuel Shih on 1/29/15.
+//  Copyright (c) 2015 Samuel Shih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
